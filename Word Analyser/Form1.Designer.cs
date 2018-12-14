07c791541138d05bbcc1165716efcf8f9cdab367
petives <peterivester@gmail.com>
2018-12-14 01:36:09 -0500

    add files

---
+++ Word Analyser/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Word_Analyser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.table = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numofWords = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numofSentences = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.avgWordLength = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numWordinSent = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.semicolons = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colons = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CmpSent = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.AcceptsReturn = true;
+            this.inputBox.AcceptsTab = true;
+            this.inputBox.Location = new System.Drawing.Point(12, 12);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(416, 371);
+            this.inputBox.TabIndex = 0;
+            this.inputBox.Text = "(Insert text here)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(171, 389);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Analyze";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // table
+            // 
+            this.table.AllowUserToAddRows = false;
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.numofWords,
+            this.numofSentences,
+            this.avgWordLength,
+            this.numWordinSent,
+            this.semicolons,
+            this.colons,
+            this.CmpSent});
+            this.table.Location = new System.Drawing.Point(434, 12);
+            this.table.Name = "table";
+            this.table.RowHeadersWidth = 50;
+            this.table.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.table.RowTemplate.Height = 24;
+            this.table.Size = new System.Drawing.Size(994, 219);
+            this.table.TabIndex = 2;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "Article Name";
+            this.id.Name = "id";
+            this.id.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.id.Width = 125;
+            // 
+            // numofWords
+            // 
+            this.numofWords.HeaderText = "Number of Words";
+            this.numofWords.Name = "numofWords";
+            this.numofWords.ReadOnly = true;
+            this.numofWords.Width = 125;
+            // 
+            // numofSentences
+            // 
+            this.numofSentences.HeaderText = "Number of Sentences";
+            this.numofSentences.Name = "numofSentences";
+            this.numofSentences.ReadOnly = true;
+            this.numofSentences.Width = 125;
+            // 
+            // avgWordLength
+            // 
+            this.avgWordLength.HeaderText = "Average Word Length";
+            this.avgWordLength.Name = "avgWordLength";
+            this.avgWordLength.ReadOnly = true;
+            this.avgWordLength.Width = 125;
+            // 
+            // numWordinSent
+            // 
+            this.numWordinSent.HeaderText = "Average Number of Words in a Sentence";
+            this.numWordinSent.Name = "numWordinSent";
+            this.numWordinSent.ReadOnly = true;
+            this.numWordinSent.Width = 125;
+            // 
+            // semicolons
+            // 
+            this.semicolons.HeaderText = "Number of Semicolons";
+            this.semicolons.Name = "semicolons";
+            this.semicolons.ReadOnly = true;
+            // 
+            // colons
+            // 
+            this.colons.HeaderText = "Number of Colons";
+            this.colons.Name = "colons";
+            this.colons.ReadOnly = true;
+            // 
+            // CmpSent
+            // 
+            this.CmpSent.HeaderText = "Number of Compound Sentences";
+            this.CmpSent.Name = "CmpSent";
+            this.CmpSent.ReadOnly = true;
+            this.CmpSent.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1462, 445);
+            this.Controls.Add(this.table);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.inputBox);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Word Analyzer";
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView table;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numofWords;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numofSentences;
+        private System.Windows.Forms.DataGridViewTextBoxColumn avgWordLength;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numWordinSent;
+        private System.Windows.Forms.DataGridViewTextBoxColumn semicolons;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colons;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CmpSent;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Word Analyser/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Word_Analyser
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int timesAnalysed = 0;
+
+        void StringTheory(string inTxt) // splits up words, gets data, and analyzes it all in one neat function
+        {
+            string[] words = inTxt.Split(' ');
+            string curWord;
+            int totalWLength = 0; //number of letters in words from the article (WILL DIVIDE BY # OF WORDS TO GET AVG WORD LENGTH)
+            int numofSentences = 0;
+            int numofWords = 0; //in the current sentence, not of the entire article
+            List<int> sLengths = new List<int>(); //numbers of words in all the sentences
+            bool sentEnd = false;
+            int CmpSents = 0; // num of compound sentences
+            int semicolons = 0;
+            int colons = 0;
+            bool hasPunc = false;
+
+            for (int i = 0; i < words.Length; i++) //gets data
+            {
+                curWord = words[i];
+                if (i > words.Length)
+                {
+                    break;
+                }
+                hasPunc = false;
+                sentEnd = false;
+
+                if (curWord.Contains('.'))
+                {
+                    hasPunc = true;
+                    if (curWord == "Dr." || curWord == "Mr." || curWord == "Mrs.") // period used as end of sentence
+                    {
+                        sentEnd = false;
+                    }
+
+                    if (curWord.Contains('0')|| curWord.Contains('1') || curWord.Contains('2') || curWord.Contains('3') || curWord.Contains('4') || curWord.Contains('5') || curWord.Contains('6') || curWord.Contains('7') || curWord.Contains('8') || curWord.Contains('9'))
+                    {
+                        sentEnd = false;
+                    }
+
+                    else sentEnd = true;
+                }
+
+                else if (curWord.Contains('?') || curWord.Contains('!'))
+                {
+                    hasPunc = true;
+                    sentEnd = true;
+                }
+
+                if (curWord.Contains(',') && (words[i + 1] == "for" || words[i + 1] == "and" || words[i + 1] == "but" || words[i + 1] == "or" || words[i + 1] == "yet" || words[i + 1] == "so"))
+                //, and a conjunction singifying a compound sentence
+                {
+                    CmpSents++;
+                }
+
+                if (curWord.Contains(';'))
+                {
+                    hasPunc = true;
+                    semicolons++;
+                    CmpSents++;
+                }
+
+
+                if (curWord.Contains(':'))
+                {
+                    hasPunc = true;
+                    colons++;
+                }
+
+                if (hasPunc) { totalWLength += curWord.Length - 1; }
+                else
+                {
+                    totalWLength += curWord.Length;
+                }
+                numofWords++;
+
+                if (sentEnd)
+                {
+                    sLengths.Add(numofWords);
+                    numofSentences++;
+                    numofWords = 0;
+                }
+            }
+
+            float avgWordLength; // average length of words
+            avgWordLength = (float)totalWLength / (float)words.Length;
+            float avgWordsInSent = 0;
+            try
+            {
+                avgWordsInSent = (float)sLengths.Sum() / (float)numofSentences;
+            }
+            catch(DivideByZeroException)
+            {
+                MessageBox.Show("Divide by zero error. Are there zero sentences?");
+                avgWordsInSent = 0;
+                numofSentences = 0;
+            }
+            List<int> dataInts = new List<int>(5)
+            {
+                words.Length, numofSentences, semicolons, colons, CmpSents
+            };
+
+            output(avgWordLength, avgWordsInSent, dataInts);
+        }
+
+        void output(float aWL, float aWIS, List<int> dataInts)
+        {
+            table.Rows.Add();
+            table.Rows[timesAnalysed].Cells[0].ReadOnly = false;
+            table.Rows[timesAnalysed].Cells[0].Value = "Enter article name here.";
+            table.Rows[timesAnalysed].Cells[1].Value = dataInts[0];
+            table.Rows[timesAnalysed].Cells[2].Value = dataInts[1];
+            table.Rows[timesAnalysed].Cells[3].ValueType = typeof(float);
+            table.Rows[timesAnalysed].Cells[3].Value = aWL;
+            table.Rows[timesAnalysed].Cells[4].ValueType = typeof(float);
+            table.Rows[timesAnalysed].Cells[4].Value = aWIS;
+            table.Rows[timesAnalysed].Cells[5].Value = dataInts[2];
+            table.Rows[timesAnalysed].Cells[6].Value = dataInts[3];
+            table.Rows[timesAnalysed].Cells[7].Value = dataInts[4];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inText = this.inputBox.Text;
+            StringTheory(inText);
+            timesAnalysed++;
+        }
+    }
+}
+
